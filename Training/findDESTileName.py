47b69ab105826ec8c5d75a0bd2b7e5d482296ed6
Annarien Bester <annarien.bester@gmail.com>
2020-03-19 15:02:42 +0200

    Getting the Tile Names of the DES2017 known lenses catalogue, from Matt

---
+++ Training/findDESTileName.py
@@ -0,0 +1,19 @@
+"""
+
+Example of how to figure out which DES tile given RA, dec coords are in.
+
+
+"""
+
+import DESTiler
+
+# Set-up of this is slow, so do only once...
+tiler=DESTiler.DESTiler("DES_DR1_TILE_INFO.csv")
+
+# How to get tile name
+RADeg, decDeg=10, -40
+tileName=tiler.getTileName(RADeg, decDeg)
+
+# How to fetch all images for tile which contains given coords
+tiler.fetchTileImages(RADeg, decDeg, 'DESTileImages')
+
