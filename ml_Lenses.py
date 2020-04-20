9b9b05f71fa78ca939d18ed2cef6a1adfda32d00
Annarien Bester <annarien.bester@gmail.com>
2020-04-20 15:39:14 +0200

    To imporve and test the KnownLenses in ml code

--- ml_Lenses.py
+++ ml_Lenses.py
@@ -50,6 +50,7 @@ def getPositiveSimulated(base_dir = 'Training/PositiveWithDESSky'):
         
         DataPos[counter] = [g, r, i] 
         counter += 1
+        # just to run, and use less things
         # if counter > 1500:
         #     break
 
@@ -80,6 +81,7 @@ def getNegativeDES(base_dir = 'Training/DES/DES_Processed'):
         i = fits.open(i_name)[0].data[0:100,0:100]    
         
         DataNeg[var] = [g, r, i]
+        # just to run, and use less things
         # if var > 1500:
         #     break
     dataSetName = 'Data Negative From DES'
@@ -113,10 +115,91 @@ def loadImage(positiveArray, negativeArray):
 
     return (np.array(image_train), np.array(image_labels))
 
+def getDES2017(base_dir = 'KnownLenses/DES2017/'):
+
+    foldersKnownDES2017 = []
+    for root, dirs, files in os.walk(base_dir):
+        for folder in dirs:
+            foldersKnownDES2017.append(os.path.join(root, folder))
+
+    nDT = len(foldersKnownDES2017)
+    DataKnownDES = np.zeros([nDT,3,100,100])
+
+    for var in range(len(foldersKnownDES2017)):
+
+        g_name = get_pkg_data_filename(foldersKnownDES2017[var]+'/g_WCSClipped.fits')
+        r_name = get_pkg_data_filename(foldersKnownDES2017[var]+'/r_WCSClipped.fits')
+        i_name = get_pkg_data_filename(foldersKnownDES2017[var]+'/i_WCSClipped.fits')    
+
+        # g_name = get_pkg_data_filename(foldersKnownDES2017[var]+'/g_norm.fits')
+        # r_name = get_pkg_data_filename(foldersKnownDES2017[var]+'/r_norm.fits')
+        # i_name = get_pkg_data_filename(foldersKnownDES2017[var]+'/i_norm.fits')    
+    
+        g = fits.open(g_name)[0].data[0:100,0:100]
+        r = fits.open(r_name)[0].data[0:100,0:100]
+        i = fits.open(i_name)[0].data[0:100,0:100]    
+        
+        DataKnownDES[var] = [g, r, i]
+
+    dataSetName = 'Known DES2017 Lenses'
+    return (DataKnownDES, dataSetName)
+
+def getJacobs(base_dir = 'KnownLenses/Jacobs_KnownLenses/'):
+    foldersKnownJacobs = []
+    for root, dirs, files in os.walk(base_dir):
+        for folder in dirs:
+            foldersKnownJacobs.append(os.path.join(root, folder))
+    nDT = len(foldersKnownJacobs)
+    DataKnownJacobs = np.zeros([nDT,3,100,100])
+
+    for var in range(len(foldersKnownJacobs)):
+
+        # g_name = get_pkg_data_filename(foldersKnownJacobs[var]+'/g_WCSClipped.fits')
+        # r_name = get_pkg_data_filename(foldersKnownJacobs[var]+'/r_WCSClipped.fits')
+        # i_name = get_pkg_data_filename(foldersKnownJacobs[var]+'/i_WCSClipped.fits')    
+
+        g_name = get_pkg_data_filename(foldersKnownJacobs[var]+'/g_norm.fits')
+        r_name = get_pkg_data_filename(foldersKnownJacobs[var]+'/r_norm.fits')
+        i_name = get_pkg_data_filename(foldersKnownJacobs[var]+'/i_norm.fits')    
+    
+        g = fits.open(g_name)[0].data[0:100,0:100]
+        r = fits.open(r_name)[0].data[0:100,0:100]
+        i = fits.open(i_name)[0].data[0:100,0:100]    
+        
+    DataKnownJacobs[var] = [g, r, i]
+    dataSetName = 'Known Jacobs Lenses'
+    return (DataKnownJacobs, dataSetName)
+
+def getUnknown(base_dir = 'KnownLenses/Unknown_Processed_256/'):
+
+
+    foldersUnknown = []
+    for root, dirs, files in os.walk(pathToUnknown):
+        for folder in dirs:
+            foldersUnknown.append(os.path.join(root, folder))
+
+    nDT = len(foldersUnknown)
+    DataUnknown = np.zeros([nDT,3,100,100])
+
+    for var in range(len(foldersUnknown)):
+        g_name = get_pkg_data_filename(foldersUnknown[var]+'/g_WCSClipped.fits')
+        r_name = get_pkg_data_filename(foldersUnknown[var]+'/r_WCSClipped.fits')
+        i_name = get_pkg_data_filename(foldersUnknown[var]+'/i_WCSClipped.fits')    
+
+        # g_name = get_pkg_data_filename(foldersUnknown[var]+'/g_norm.fits')
+        # r_name = get_pkg_data_filename(foldersUnknown[var]+'/r_norm.fits')
+        # i_name = get_pkg_data_filename(foldersUnknown[var]+'/i_norm.fits')    
+
+        g = fits.open(g_name)[0].data[0:100,0:100]
+        r = fits.open(r_name)[0].data[0:100,0:100]
+        i = fits.open(i_name)[0].data[0:100,0:100]    
+        
+        DataUnknown[var] = [g, r, i]
+
+    dataSetName = 'Unknown Images'
+    return (DataUnknown, dataSetName)
 #_____________________________________________________________________________________________________________________________
 # MAIN
-# positiveArray = []
-# negativeArray = []
 
 positiveArray, posName = getPositiveSimulated()
 negativeArray, negName = getNegativeDES()
@@ -135,15 +218,23 @@ checkParameters('ImageTrain' , imageTrain)
 print('Shape of ImageLabels: %s' %(imageLabels.shape))
 
 # add dimension to imageTrain
-print("10 in ImageTrain: %s" %(imageTrain[10]))
-a = imageTrain.squeeze()
-print("Shape of A: %s " %(a[10]))
+# print("10 in ImageTrain: %s" %(imageTrain[10]))
+
 # checking by plotting image
-plt.imshow(a)
+# plt.imshow(imageTrain[10])
+im2disp = imageTrain[10].transpose((1,2,0)) # changed 0,1,2,3 array to 0,1,2 for images(this is now from 10000,3, 100, 100, to 3,100,10000 )
+plt.imshow(im2disp)
+plt.show()
 print('Label: ' , imageLabels[10])
 
 # reshape X
-X = imageTrain.reshape(imageTrain.shape[0], imageTrain[1]*imageTrain[2],imageTrain[3]) # batchsize, height*width*3channels
+print("ImageTrain 0 : %s  and TYPE: %s" % (imageTrain.shape[0], str(type(imageTrain.shape[0]))))
+print("ImageTrain 1 : %s  and TYPE: %s" % (imageTrain.shape[1], str(type(imageTrain.shape[1]))))
+print("ImageTrain 2 : %s  and TYPE: %s" % (imageTrain.shape[2], str(type(imageTrain.shape[2]))))
+print("ImageTrain 3 : %s  and TYPE: %s" % (imageTrain.shape[3], str(type(imageTrain.shape[3]))))
+
+
+X = imageTrain.reshape(imageTrain.shape[0], imageTrain.shape[1]*imageTrain.shape[2]*imageTrain.shape[3]) # batchsize, height*width*3channels
 print("Shape of X: "+str(X.shape))
 
 # Encoding Y now
@@ -167,9 +258,47 @@ clf_image = MLPClassifier(activation = 'relu',
 clf_image.fit(x_train, y_train)
 
 y_pred = clf_image.predict(x_test)
-accuracy_score(y_test, y_pred)
+y_accuracy = accuracy_score(y_test, y_pred)
 
+print("clf_image: "+str(clf_image))
 print("Y_pred: " + str(y_pred))
-print("Accuracy_Score: " +str(accuracy_score))
+print("Accuracy_Score: " +str(y_accuracy))
+
+#______________________________________________________________________________________________________________________
+knownDES2017Array,des2017Name = getDES2017()
+knownJacobsArray, jacobsName = getJacobs()
+unknownArray, unknownName = getUnknown()
+
+checkParameters(des2017Name, knownDES2017Array)
+checkParameters(jacobsName, knownJacobsArray)
+checkParameters(unknownName, unknownArray)
+
+imageTest, labelsTest = loadImage(knownDES2017Array, unknownArray)
+
+checkParameters('Image DES2017 Test' , imageTest)
+print('Shape of Image DES2017 Labels: %s' %(labelsTest.shape))
+
+imageAccuracy = accuracy_score(imageTest, y_pred)
+print("Image DES2017 Accuracy: " + str(imageAccuracy))
+
+imageJacobsTest, labelsJacobsTest = loadImage(knownJacobsArray, unknownArray)
+
+checkParameters('Image Jacobs Test: %s' % ( imageJacobsTest))
+print('Shape of Image Jacobs Labels: %s' % (labelsJacobsTest.shape))
+
+imageJacobsAccuracy = accuracy_score(imageTest, y_pred)
+print("Image DES2017 Accuracy: " + str(imageJacobsAccuracy))
+
+allKnownArray, allKnownName = np.vstack(knownDES2017Array, knownJacobsArray)
+checkParameters(allKnownName, allKnownArray)
+
+imageKnownTest, labelsKnownTest = loadImage(allKnownArray, unknownArray)
+checkParameters('Image All Known Test: %s' % ( imageKnownTest))
+print('Shape of Image All Known Labels: %s' % (labelsKnownTest.shape))
+
+imageAllKnownAccuracy = accuracy_score(imageKnownTest, y_pred)
+print("Image DES2017 Accuracy: " + str(imageAllKnownAccuracy))
+
+
 
 
