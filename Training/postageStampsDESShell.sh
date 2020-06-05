286f82298ad2fb63e24b450226c18be17b954421
Annarien Bester <annarien.bester@gmail.com>
2020-06-05 13:32:41 +0200

    Removing Excess things that arent being used anymore

--- Training/postageStampsDESShell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=<# tasks per node>
-#SBATCH --mem=<memory in Mb, max is 64000>
-#SBATCH --time=<time in HH:MM:SS format, max is 24:00:00>
-
-source /home/mjh/SETUP_PY27.sh
-module load python/2.7.8_gcc-4.4.7
-python postageStampsDES.py
