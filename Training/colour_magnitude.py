83f45cfd0e15382f8a075387c0209dd3e9ad0968
Annarien Bester <annarien.bester@gmail.com>
2020-10-04 12:31:57 +0200

    A colour magnitude diagram was made. Axis needs to be fixed, sort it in acending order

--- Training/colour_magnitude.py
+++ Training/colour_magnitude.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Global Variables
+from matplotlib.ticker import LinearLocator
+
 train_positive = 'Training/g_r_PositiveAll'
 test_positive = 'Testing/g_r_PositiveAll'
 
@@ -36,12 +38,12 @@ def getMagnitudeTable(positive_path):
 
         for line in csvfile.readlines():
             array = line.split(',')
-            first_item = array[0]
-            print(first_item)
 
             num = array[0]
             lens_g_mag = array[1]
             lens_r_mag = array[2]
+            print(lens_g_mag)
+            print(lens_r_mag)
             lens_i_mag = array[3]
             source_g_mag = array[4]
             source_r_mag = array[5]
@@ -54,7 +56,7 @@ def getMagnitudeTable(positive_path):
             # source_gi =array ['Source_gi']
 
             lens_r_list.append(lens_r_mag)
-            print(type(lens_r_list))
+            # print(type(lens_r_list))
             lens_gr_list.append(lens_gr)
             source_r_list.append(source_r_mag)
             source_gr_list.append(source_gr)
@@ -72,33 +74,66 @@ def colourMagnitudeDiagram(lens_r_list, lens_gr_list, source_r_list, source_gr_l
         source_gr_list(list):   This list contains the g-r band magnitudes for the sources.
 
     """
-    fig, ax = plt.subplots()
+    fig = plt.figure()
     lens_r_list = lens_r_list[1:]
     lens_gr_list = lens_gr_list[1:]
     source_r_list = source_r_list[1:]
     source_gr_list = source_gr_list[1:]
 
+    # GETTING THE MIN AND MAX:
+    lens_r_min = min(lens_r_list)
+    lens_r_max = max(lens_r_list)
+    lens_gr_min = min(lens_gr_list)
+    lens_gr_max = max(lens_gr_list)
+    source_r_min = min(source_r_list)
+    source_r_max = max(source_r_list)
+    source_gr_min = min(source_gr_list)
+    source_gr_max = max(source_gr_list)
+
+    r_min = min(lens_r_min,source_r_min)
+    r_max = max(lens_r_max,source_r_max)
+    gr_min = min(lens_gr_min, source_gr_min)
+    gr_max = max(lens_gr_max,source_gr_max)
+
     x = lens_gr_list
+    print(x)
     y = lens_r_list
-    ax.scatter(x, y, c='blue', label='Lenses')
     x2 = source_gr_list
     y2 = source_r_list
-    ax.scatter(x2, y2, c='red', label='Sources')
 
-    ax.set_xlabel('g-r')
-    ax.set_ylabel('r')
+    plt.scatter(x, y, c='blue', label='Lenses')
+    plt.scatter(x2, y2, color='red', label='Sources')
+    plt.xlabel('g-r')
+    plt.ylabel('r')
 
-    max_x = max(source_gr_list)
-    print(max_x)
-    max_y = max(source_r_list)
-    print(max_y)
+    axes = plt.axes()
+    # x_min = min(float(gr_min), float(gr_max))
+    # print(x_min)
+    # # x_max = max(float(gr_min), float(gr_max))
+    # print((x_max))
+    x_min = float(gr_min)
+    x_max = float(gr_max)
+    print(x_min)
+    print(x_max)
 
-    ax.set_xticks(ax.get_xticks()[::16])
-    ax.set_yticks(ax.get_yticks()[::16])
+    # y_min = min(float(r_min), float(r_max))
+    y_min = float(r_min)
+    y_max = float(r_max)
+    print(y_min)
+    print(y_max)
 
-    ax.legend()
-    ax.grid(True)
+    # y_max = max(float(r_min), float(r_max))
+    # axes.set_xlim(0, 3)
+    # axes.set_ylim(16, 22)
+    # axes.set(xlim=(x_min, x_max), ylim=(y_min, y_max))
+    axes.get_yaxis().set_major_locator(LinearLocator(numticks=10))
+    axes.get_xaxis().set_major_locator(LinearLocator(numticks=10))
 
+    if positive_path == 'Training/g_r_PositiveAll':
+        plt.title('Training Colour Magnitude')
+    elif positive_path == 'Testing/g_r_PositiveAll':
+        plt.title('Testing Colour Magnitude')
+    plt.legend()
     plt.show()
     fig.savefig('%s_colourMagnitudeDiagram.png' % positive_path)
 
@@ -111,4 +146,3 @@ colourMagnitudeDiagram(train_lens_r, train_lens_gr, train_source_r, train_source
 
 test_lens_r, test_lens_gr, test_source_r, test_source_gr = getMagnitudeTable(test_positive)
 colourMagnitudeDiagram(test_lens_r, test_lens_gr, test_source_r, test_source_gr, positive_path=test_positive)
-
