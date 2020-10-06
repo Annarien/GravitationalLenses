6fdcefd0edff7273bfe026764423bc1addafea46
Annarien Bester <annarien.bester@gmail.com>
2020-10-06 08:11:07 +0200

    Fixing files for Hep

--- For_Hep/FixedKeras/KerasCnn_g_r.py
+++ For_Hep/FixedKeras/KerasCnn_g_r.py
@@ -28,14 +28,14 @@ excel_headers.append("Date and Time")
 excel_dictionary.append(dt_string)
 
 # Globals
-max_num = 500  # Set to sys.maxsize when running entire data set
-max_num_testing = 1000  # sys.maxsize  # Set to sys.maxsize when running entire data set
-max_num_prediction = 1000  # sys.maxsize  # Set to sys.maxsize when running entire data set
+max_num = sys.maxsize  # Set to sys.maxsize when running entire data set
+max_num_testing = sys.maxsize  # Set to sys.maxsize when running entire data set
+max_num_prediction = sys.maxsize  # Set to sys.maxsize when running entire data set
 validation_split = 0.2  # A float value between 0 and 1 that determines what percentage of the training
 # data is used for validation.
 k_fold_num = 5  # A number between 1 and 10 that determines how many times the k-fold classifier
 # is trained.
-epochs = 20  # A number that dictates how many iterations should be run to train the classifier
+epochs = 200  # A number that dictates how many iterations should be run to train the classifier
 batch_size = 128  # The number of items batched together during training.
 run_k_fold_validation = False  # Set this to True if you want to run K-Fold validation as well.
 input_shape = (100, 100, 3)  # The shape of the images being learned & evaluated.
@@ -472,8 +472,8 @@ executeKFoldValidation(training_data,
                        excel_dictionary)
 
 # Plot run metrics
-acc = history.history['acc']
-val_acc = history.history['val_acc']
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 number_of_completed_epochs = range(1, len(acc) + 1)
